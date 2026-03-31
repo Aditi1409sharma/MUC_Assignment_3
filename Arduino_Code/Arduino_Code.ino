584275a0b0c328e2bd7ccba4537914be3c94a84a
dhruv230605 <dhruvjainsat@gmail.com>
2026-03-31 17:25:09 +0530

    Added Arduino Code

---
+++ Arduino_Code/Arduino_Code.ino
@@ -0,0 +1,32 @@
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define LDR_PIN A0
+
+void setup() {
+  Serial.begin(9600); // Standard baud rate for HC-05
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  // 1. Measure Distance (Ultrasonic)
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.034 / 2;
+
+  // 2. Measure Light (LDR)
+  int lightValue = analogRead(LDR_PIN);
+
+  // 3. Send Data to Android via HC-05
+  // Format: "distance,light" -> e.g., "45.2,320"
+  Serial.print(distance);
+  Serial.print(",");
+  Serial.println(lightValue);
+
+  delay(200); // Send data 5 times per second
+}
